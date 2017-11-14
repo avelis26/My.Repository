248b6dc9b34cddca9935f15379e15a0dfa4540bc
Graham Pinkston <graham.pinkston@gmail.com>
2017-11-13 19:30:09 -0600

    init

--- Server.Firewall/Edit-NicIpConfig.ps1
+++ Server.Firewall/Edit-NicIpConfig.ps1
@@ -6,3 +6,13 @@ $user = "gpink003@7-11.com"
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
 Login-AzureRmAccount -Credential $credential
 Set-AzureRmContext -Subscription 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
+$vmList = Get-AzureRmVM | Where-Object -FilterScript {$_.Name -like "mssslcrmfw*"}
+ForEach ($vm in $vmList) {
+	$nics = $vm.NetworkProfile.NetworkInterfaces
+	ForEach ($nic in $nics) {
+		$nicName = [regex]::Split($nic.Id, '^.+\/(.+)$')
+		$nicObj = Get-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $vm.ResourceGroupName
+	}
+	Write-Host 'god just killed a kitten'
+	#Get-AzureRmNetworkInterfaceIpConfig
+}
