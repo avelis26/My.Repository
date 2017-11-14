adaa50f2db7788690a81ecc85daa3fe05f83d21f
Graham Pinkston <graham.pinkston@gmail.com>
2017-11-13 18:10:56 -0600

    init

---
+++ Server.Firewall/Edit-NicIpConfig.ps1
@@ -0,0 +1,6 @@
+Import-Module AzureRM
+$password = Get-Content "C:\Users\graham.pinkston\Documents\Secrets\op1.txt" | ConvertTo-SecureString
+$user = "gpink003@7-11.com"
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
+Login-AzureRmAccount -Credential $credential
+Set-AzureRmContext -Subscription 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
