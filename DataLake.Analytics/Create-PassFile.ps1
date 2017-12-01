e0d45e9dc6fbab183e3b4906edeec075f1ef4895
Graham Pinkston <graham.pinkston@gmail.com>
2017-11-30 22:15:57 -0600

    init

--- DataLake.Analytics/Create-PassFile.ps1
+++ DataLake.Analytics/Create-PassFile.ps1
@@ -1,8 +1,12 @@
 #####################################################################################
-$clrPass = '' ##-Don't save file with password, clear before save-##
+$clrPass = '9uNb0u|\|d><2600@' ##-Don't save file with password, clear before save-##
 #####################################################################################
 $passFileName = 'op1.txt'
 $passFileRootPath = 'C:\Users\graham.pinkston\Documents\Secrets\'
+If (!(Test-Path -LiteralPath $passFileRootPath)) {
+	Write-Verbose -Message "Creating folder:  $passFileRootPath"
+	New-Item -ItemType Directory -Path $passFileRootPath -Force
+}
 $secPass = ConvertTo-SecureString -String $clrPass -AsPlainText -Force
 $encPass = ConvertFrom-SecureString -SecureString $secPass
 Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName)
--- DataLake.Analytics/Get-DataLakeFiles.ps1
+++ DataLake.Analytics/Get-DataLakeFiles.ps1
@@ -1,20 +1,33 @@
+$VerbosePreference = 'Continue'
 Import-Module AzureRM
 $password = Get-Content "C:\Users\graham.pinkston\Documents\Secrets\op1.txt" | ConvertTo-SecureString
 $user = "gpink003@7-11.com"
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
 $seiDataLakeName = '711dlprodcons01'
 $ansiraDataLakeName = 'mscrmprodadls'
-$dataLakeRootPath = '/BIT_CRM/20171129/'
+$targetFolder = '20171129'
+$dataLakeRootPath = "/BIT_CRM/$targetFolder/"
+$destinationRootPath = 'C:\Temp\'
+$i = 0
 Try {
+	Write-Verbose -Message 'Logging into Azure...'
 	Login-AzureRmAccount -ErrorAction Stop
+	Write-Verbose -Message 'Setting subscription...'
 	Set-AzureRmContext -Subscription 'ee691273-18af-4600-bc24-eb6768bf9cfa' -ErrorAction Stop
+	Write-Verbose -Message "Getting list of files in $dataLakeRootPath ..."
 	$dataLakeFiles = Get-AzureRmDataLakeStoreChildItem -Account $seiDataLakeName -Path $dataLakeRootPath -ErrorAction Stop
-	Write-Output $dataLakeFiles[0]
-	Write-Output $dataLakeFiles[1]
-	Write-Output $dataLakeFiles[2]
+	Write-Verbose -Message "$($dataLakeFiles.Length) files found..."
+	$percent = $($i/$($dataLakeFiles.Length)*100)
+	If (!(Test-Path -LiteralPath $destinationRootPath)) {
+		Write-Verbose -Message "Creating folder:  $destinationRootPath ..."
+		New-Item -ItemType Directory -Path $destinationRootPath -Force
+	}
+	ForEach ($file in $dataLakeFiles) {
+		Write-Progress -Activity 'Downloading files...' -Status "$($percent.ToString("##"))% Complete:" -PercentComplete $percent
+		Export-AzureRmDataLakeStoreItem -Account $seiDataLakeName -Path $file.Path -Destination $($destinationRootPath + $targetFolder + '\' + $($file.Name))
+		$i++
+	}
 }
 Catch {
 	throw $_
 }
-
-# Export-AzureRmDataLakeStoreItem -AccountName $dataLakeStoreName -Path $myrootdir\mynewdirectory\vehicle1_09142014_Copy.csv -Destination "C:\sampledata\vehicle1_09142014_Copy.csv"
