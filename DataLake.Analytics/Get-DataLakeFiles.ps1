f7288989e0f54a65e51f70793cf1e2414b5c0655
Graham Pinkston <graham.pinkston@gmail.com>
2017-11-30 22:16:28 -0600

    dev

--- DataLake.Analytics/Get-DataLakeFiles.ps1
+++ DataLake.Analytics/Get-DataLakeFiles.ps1
@@ -11,7 +11,7 @@ $destinationRootPath = 'C:\Temp\'
 $i = 0
 Try {
 	Write-Verbose -Message 'Logging into Azure...'
-	Login-AzureRmAccount -ErrorAction Stop
+	Login-AzureRmAccount -Credential $credential -ErrorAction Stop
 	Write-Verbose -Message 'Setting subscription...'
 	Set-AzureRmContext -Subscription 'ee691273-18af-4600-bc24-eb6768bf9cfa' -ErrorAction Stop
 	Write-Verbose -Message "Getting list of files in $dataLakeRootPath ..."
