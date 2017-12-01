a5f2942d0bd5f378ce57668b09576937cf48e2b6
Graham Pinkston <graham.pinkston@gmail.com>
2017-12-01 17:23:44 -0600

    init

---
+++ AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
@@ -0,0 +1,10 @@
+﻿$clrPass = Read-Host -Prompt "Enter password:"
+$passFileName = 'op1.txt'
+$passFileRootPath = 'C:\Users\graham.pinkston\Documents\Secrets\'
+If (!(Test-Path -LiteralPath $passFileRootPath)) {
+	Write-Verbose -Message "Creating folder:  $passFileRootPath"
+	New-Item -ItemType Directory -Path $passFileRootPath -Force
+}
+$secPass = ConvertTo-SecureString -String $clrPass -AsPlainText -Force
+$encPass = ConvertFrom-SecureString -SecureString $secPass
+Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName)
