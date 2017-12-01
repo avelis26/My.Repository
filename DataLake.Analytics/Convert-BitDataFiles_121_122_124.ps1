f59913f5902ff83efdff2617462a7641b10c88ab
Graham Pinkston <graham.pinkston@gmail.com>
2017-12-01 00:16:39 -0600

    dev

---
+++ DataLake.Analytics/Convert-BitDataFiles_121_122_124.ps1
@@ -0,0 +1,3 @@
+#
+# Convert_BitDataFiles_121_122_124.ps1
+#
--- DataLake.Analytics/Get-DataLakeFiles.ps1
+++ DataLake.Analytics/Get-DataLakeFiles.ps1
@@ -1,5 +1,4 @@
 $VerbosePreference = 'Continue'
-Import-Module AzureRM
 $password = Get-Content "C:\Users\graham.pinkston\Documents\Secrets\op1.txt" | ConvertTo-SecureString
 $user = "gpink003@7-11.com"
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
@@ -10,6 +9,8 @@ $dataLakeRootPath = "/BIT_CRM/$targetFolder/"
 $destinationRootPath = 'C:\Temp\'
 $i = 0
 Try {
+	Write-Verbose -Message 'Importing AzureRm module...'
+	Import-Module AzureRM
 	Write-Verbose -Message 'Logging into Azure...'
 	Login-AzureRmAccount -Credential $credential -ErrorAction Stop
 	Write-Verbose -Message 'Setting subscription...'
@@ -17,12 +18,12 @@ Try {
 	Write-Verbose -Message "Getting list of files in $dataLakeRootPath ..."
 	$dataLakeFiles = Get-AzureRmDataLakeStoreChildItem -Account $seiDataLakeName -Path $dataLakeRootPath -ErrorAction Stop
 	Write-Verbose -Message "$($dataLakeFiles.Length) files found..."
-	$percent = $($i/$($dataLakeFiles.Length)*100)
 	If (!(Test-Path -LiteralPath $destinationRootPath)) {
 		Write-Verbose -Message "Creating folder:  $destinationRootPath ..."
 		New-Item -ItemType Directory -Path $destinationRootPath -Force
 	}
 	ForEach ($file in $dataLakeFiles) {
+		$percent = $($i/$($dataLakeFiles.Length)*100)
 		Write-Progress -Activity 'Downloading files...' -Status "$($percent.ToString("##"))% Complete:" -PercentComplete $percent
 		Export-AzureRmDataLakeStoreItem -Account $seiDataLakeName -Path $file.Path -Destination $($destinationRootPath + $targetFolder + '\' + $($file.Name))
 		$i++
