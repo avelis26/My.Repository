ff6a59d74dc716661f84cc7dc6c504d271aec523
Graham Pinkston <graham.pinkston@gmail.com>
2017-12-04 14:54:49 -0600

    fixed cmdlet typo

--- AzureRM.Generic.Tools/Create-DbCredObj.ps1
+++ AzureRM.Generic.Tools/Create-DbCredObj.ps1
@@ -1,4 +1,5 @@
 Clear-Host
+Import-Module AzureRm
 Login-AzureRmAccount
 Set-AzureRmContext –SubscriptionID 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
 $encPass = Get-Content -Path 'C:\Users\graham.pinkston\Documents\Secrets\op1.txt'
@@ -11,4 +12,4 @@ $params = {
     Secret = $mysecret
     AccountName = 'mscrmprodadla'
 }
-New-AzureDataLakeAnalyticsCatalogSecret @params
+New-AzureRmDataLakeAnalyticsCatalogSecret @params
