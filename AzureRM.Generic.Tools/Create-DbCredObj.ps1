113f78f333bad4bff404226b3d11a0329eab5155
Graham Pinkston <graham.pinkston@gmail.com>
2017-12-08 16:57:14 -0600

    dev

--- AzureRM.Generic.Tools/Create-DbCredObj.ps1
+++ AzureRM.Generic.Tools/Create-DbCredObj.ps1
@@ -4,13 +4,29 @@ Login-AzureRmAccount
 Set-AzureRmContext -SubscriptionID 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
 $encPass = Get-Content -Path 'C:\Users\graham.pinkston\Documents\Secrets\op1.txt'
 $passwd = ConvertTo-SecureString $encPass
-$cred = New-Object System.Management.Automation.PSCredential('gpink003@7-11.com', $passwd)
-$params = @{
-	Account = 'mscrmprodadla';    
+$cred1 = New-Object System.Management.Automation.PSCredential('gpink003@7-11.com', $passwd)
+$cred2 = New-Object System.Management.Automation.PSCredential('gpink003@7-11.com@msprodsqldw', $passwd)
+$createParams1 = @{
+	Account = 'mscrmprodadla';
 	DatabaseName = 'master';
 	CredentialName = 'gpink003';
 	Credential = $cred;
 	DatabaseHost = 'msprodsqldw.database.windows.net';
 	Port = 1433
 }
-New-AzureRmDataLakeAnalyticsCatalogCredential @params
+$createParams2 = @{
+	Account = 'mscrmprodadla';
+	DatabaseName = 'master';
+	CredentialName = 'gpink003_instance';
+	Credential = $cred;
+	DatabaseHost = 'msprodsqldw.database.windows.net';
+	Port = 1433
+}
+$removeParams = @{
+	Account = 'mscrmprodadla';
+	DatabaseName = 'master';
+	Name = 'gpink003';
+}
+Remove-AzureRmDataLakeAnalyticsCatalogCredential @removeParams
+Start-Sleep -Seconds 3
+New-AzureRmDataLakeAnalyticsCatalogCredential @createParams2
