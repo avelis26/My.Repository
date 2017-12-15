2fc5e0cd15caab6e2627a6682ab2c85d4950aa3c
Graham Pinkston <graham.pinkston@gmail.com>
2017-12-15 14:19:58 -0600

    minor fixes in error handling

--- AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
+++ AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
@@ -4,13 +4,19 @@
 $userName = 'gpink003'
 #######################################################################################################
 #######################################################################################################
-$passFileName = "$userName.cred"
-$passFileRootPath = "C:\Users\$userName\Documents\Secrets\"
-If (!(Test-Path -LiteralPath $passFileRootPath)) {
-	Write-Verbose -Message "Creating folder:  $passFileRootPath"
-	New-Item -ItemType Directory -Path $passFileRootPath -Force
+Try {
+	$passFileName = "$userName.cred"
+	$passFileRootPath = "C:\Users\$userName\Documents\Secrets\"
+	If (!(Test-Path -LiteralPath $passFileRootPath)) {
+		Write-Verbose -Message "Creating folder:  $passFileRootPath"
+		New-Item -ItemType Directory -Path $passFileRootPath -Force -ErrorAction Stop
+	}
+	$secPass = Read-Host -Prompt "Please type your 7-11 password and hit 'Enter'..." -AsSecureString
+	$encPass = ConvertFrom-SecureString -SecureString $secPass
+	Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName) -ErrorAction Stop
+	Write-Output $($passFileRootPath + $passFileName)
 }
-$secPass = Read-Host -Prompt "Please type your 7-11 password and hit 'Enter'..." -AsSecureString
-$encPass = ConvertFrom-SecureString -SecureString $secPass
-Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName)
-Write-Output $($passFileRootPath + $passFileName)
+Catch {
+	throw $_
+	Write-Host -ForegroundColor Magenta 'Run PowerShell ISE as Administrator'
+}
