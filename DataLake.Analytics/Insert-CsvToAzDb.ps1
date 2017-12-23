648bd03dd198e08c480707320d91f3cd815df2c6
Graham Pinkston <graham.pinkston@gmail.com>
2017-12-23 14:51:21 -0600

    removed comments

--- DataLake.Analytics/Insert-CsvToAzDb.ps1
+++ DataLake.Analytics/Insert-CsvToAzDb.ps1
@@ -7,7 +7,6 @@ $table = 'test'
 $user = 'sqladmin'
 $pass = 'Password20!7!'
 $server = 'mstestsqldw.database.windows.net'
-#$command = "bcp $($database).$($schema).$($table) in $($file) -c -U $($user)@$($server) -S tcp:$($server).database.windows.net -P $($pass)"
 $command = "bcp $table in $file -S $server -d $database -U $user -P $pass -q -c -t ',' -F 2"
 Write-Debug -Message $command
 Invoke-Expression -Command $command -Verbose
