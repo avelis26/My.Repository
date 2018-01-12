93116f2a35fe77daf45f74588f263772d454de6a
Graham Pinkston <graham.pinkston@gmail.com>
2018-01-12 01:10:05 -0600

    init

---
+++ DataLake.Analytics/Create-BitStoreReport.ps1
@@ -0,0 +1,13 @@
+# Get raw files
+
+# Seperate files into groups of 1000 files in seperate folders
+
+# Execute C# app as job 
+
+# Call SP to clean DB of rows older than 90 days
+
+# Insert output to DB
+
+# Call SP to run aggregate queries
+
+# Clean up, send notifications
