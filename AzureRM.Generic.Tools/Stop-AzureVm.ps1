3ac4f41ccc1fc517f09150f3d33bdbc407c4bf66
Graham Pinkston <graham.pinkston@gmail.com>
2018-01-23 16:20:46 -0600

    dev

--- AzureRM.Generic.Tools/Stop-AzureVm.ps1
+++ AzureRM.Generic.Tools/Stop-AzureVm.ps1
@@ -1,29 +1,55 @@
 $server = 'ms-ssw-crm-bitc'
-$currentUser = $env:USERNAME
-$userName = 'gpink003@7-11.com'
+$resourceGroup = 'CRM-Production-RG'
+$user = $userName + '@7-11.com'
+$userName = 'gpink003'
 $smtpServer = '10.128.1.125'
 $port = 25
 $fromAddr = 'noreply@7-11.com'
 $emailList = 'graham.pinkston@ansira.com'
-add-type @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
+$policy = [System.Net.ServicePointManager]::CertificatePolicy.ToString()
+If ($policy -ne 'TrustAllCertsPolicy') {
+	add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+	public bool CheckValidationResult(
+		ServicePoint srvPoint, X509Certificate certificate,
+		WebRequest request, int certificateProblem
+	) {
+		return true;
+	}
+}
 "@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+	[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
 $params = @{
 	SmtpServer = $smtpServer;
 	Port = $port;
 	UseSsl = 0;
 	From = $fromAddr;
 	To = $emailList;
-	Subject = "$currentUser Has Initiated $server Shutdown";
+	Subject = "$env:USERNAME Has Initiated $server Shutdown";
 	Body = "$server is shutting down."
+}
+Send-MailMessage @params
+Import-Module AzureRM -ErrorAction Stop
+$password = ConvertTo-SecureString -String $(Get-Content -Path "C:\Users\$userName\Documents\Secrets\$userName.cred")
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
+Login-AzureRmAccount -Credential $credential -Subscription 'da908b26-f6f8-4d61-bf60-b774ff3087ec' -ErrorAction Stop
+$continue = Read-Host -Prompt "Are you super super sure you want to SHUTDOWN $server? (y/n)"
+If ($continue -eq 'y') {
+	Stop-AzureRmVM -ResourceGroupName $resourceGroup -Name $server -Force
+}
+Else {
+	Write-Output "You choose something other than 'y' sooooo have a nice day :)"
+}
+$params = @{
+	SmtpServer = $smtpServer;
+	Port = $port;
+	UseSsl = 0;
+	From = $fromAddr;
+	To = $emailList;
+	Subject = "$server Shutdown Successful";
+	Body = "Have a nice day."
 }
 Send-MailMessage @params
