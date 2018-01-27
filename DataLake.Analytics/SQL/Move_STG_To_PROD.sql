2b0dd99ee7cb12d052c3d7bd9ff9ee6fe26d7926
Graham Pinkston <graham.pinkston@gmail.com>
2018-01-27 10:46:22 -0600

    init

---
+++ DataLake.Analytics/SQL/Move_STG_To_PROD.sql
@@ -0,0 +1,158 @@
+USE								[7ELE]
+GO
+IF EXISTS						(SELECT * FROM sys.procedures WHERE name = 'usp_Move_STG_To_PROD')
+BEGIN
+DROP PROCEDURE					[dbo].[usp_Move_STG_To_PROD]
+END
+GO
+CREATE PROCEDURE				[dbo].[usp_Move_STG_To_PROD]
+AS
+SET NOCOUNT ON
+INSERT INTO						[dbo].[prod_121_Header]
+(
+								[RecordId],
+								[StoreNumber],
+								[TransactionType],
+								[DayNumber],
+								[ShiftNumber],
+								[TransactionUID],
+								[Aborted],
+								[DeviceNumber],
+								[DeviceType],
+								[EmployeeNumber],
+								[EndDate],
+								[EndTime],
+								[StartDate],
+								[StartTime],
+								[Status],
+								[TotalAmount],
+								[TransactionCode],
+								[TransactionSequence],
+								[RewardMemberID]
+)
+SELECT
+								[RecordId],
+								[StoreNumber],
+								[TransactionType],
+								[DayNumber],
+								[ShiftNumber],
+								[TransactionUID],
+								[Aborted],
+								[DeviceNumber],
+								[DeviceType],
+								[EmployeeNumber],
+								[EndDate],
+								[EndTime],
+								[StartDate],
+								[StartTime],
+								[Status],
+								[TotalAmount],
+								[TransactionCode],
+								[TransactionSequence],
+								[RewardMemberID]
+FROM							[dbo].[stg_121_Headers]
+GO
+TRUNCATE TABLE					[dbo].[stg_121_Headers]
+GO
+INSERT INTO						[dbo].[prod_122_Details]
+(
+								[RecordID],
+								[StoreNumber],
+								[TransactionType],
+								[DayNumber],
+								[ShiftNumber],
+								[TransactionUID],
+								[SequenceNumber],
+								[ProductNumber],
+								[PLUNumber],
+								[RecordAmount],
+								[RecordCount],
+								[RecordType],
+								[SizeIndx],
+								[ErrorCorrectionFlag],
+								[PriceOverideFlag],
+								[TaxableFlag],
+								[VoidFlag],
+								[RecommendedFlag],
+								[PriceMultiple],
+								[CarryStatus],
+								[TaxOverideFlag],
+								[PromotionCount],
+								[SalesPrice],
+								[MUBasePrice],
+								[HostItemId],
+								[CouponCount]
+)
+SELECT
+								[RecordID],
+								[StoreNumber],
+								[TransactionType],
+								[DayNumber],
+								[ShiftNumber],
+								[TransactionUID],
+								[SequenceNumber],
+								[ProductNumber],
+								[PLUNumber],
+								[RecordAmount],
+								[RecordCount],
+								[RecordType],
+								[SizeIndx],
+								[ErrorCorrectionFlag],
+								[PriceOverideFlag],
+								[TaxableFlag],
+								[VoidFlag],
+								[RecommendedFlag],
+								[PriceMultiple],
+								[CarryStatus],
+								[TaxOverideFlag],
+								[PromotionCount],
+								[SalesPrice],
+								[MUBasePrice],
+								[HostItemId],
+								[CouponCount]						
+FROM							[dbo].[stg_122_Details]
+GO
+TRUNCATE TABLE					[dbo].[stg_122_Details]
+GO
+INSERT INTO						[dbo].[prod_124_Media]
+(
+								[RecordId],
+								[StoreNumber],
+								[TransactionType],
+								[DayNumber],
+								[ShiftNumber],
+								[TransactionUID],
+								[SequenceNumber],
+								[MediaNumber],
+								[NetworkMediaSequenceNumber],
+								[MediaType],
+								[RecordCount],
+								[RecordAmount],
+								[RecordType],
+								[ErrorCorrectionFlag],
+								[VoidFlag],
+								[ExchangeRate],
+								[ForeignAmount]
+)
+SELECT
+								[RecordId],
+								[StoreNumber],
+								[TransactionType],
+								[DayNumber],
+								[ShiftNumber],
+								[TransactionUID],
+								[SequenceNumber],
+								[MediaNumber],
+								[NetworkMediaSequenceNumber],
+								[MediaType],
+								[RecordCount],
+								[RecordAmount],
+								[RecordType],
+								[ErrorCorrectionFlag],
+								[VoidFlag],
+								[ExchangeRate],
+								[ForeignAmount]
+FROM							[dbo].[stg_124_Media]
+GO
+TRUNCATE TABLE					[dbo].[stg_124_Media]
+GO
