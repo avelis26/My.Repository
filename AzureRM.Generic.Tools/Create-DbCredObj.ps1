dcea0418cff43f2dac383568660353f793f6db60
Graham Pinkston <graham.pinkston@gmail.com>
2018-02-21 12:03:03 -0600

    updated

--- AzureRM.Generic.Tools/Create-DbCredObj.ps1
+++ AzureRM.Generic.Tools/Create-DbCredObj.ps1
@@ -1,32 +1,26 @@
 Clear-Host
-Import-Module AzureRm
-Login-AzureRmAccount
-Set-AzureRmContext -SubscriptionID 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
-$encPass = Get-Content -Path 'C:\Users\graham.pinkston\Documents\Secrets\op1.txt'
-$passwd = ConvertTo-SecureString $encPass
-$cred1 = New-Object System.Management.Automation.PSCredential('gpink003@7-11.com', $passwd)
-$cred2 = New-Object System.Management.Automation.PSCredential('gpink003@7-11.com@msprodsqldw', $passwd)
-$createParams1 = @{
+Import-Module AzureRm -ErrorAction Stop
+Login-AzureRmAccount -Subscription 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
+$encPass = Get-Content -Path 'C:\Users\gpink003\Documents\Secrets\sqladmin.cred'
+$passwd = ConvertTo-SecureString $encPass -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential('sqladmin', $passwd)
+$credParams = @{
 	Account = 'mscrmprodadla';
 	DatabaseName = 'master';
-	CredentialName = 'gpink003';
+	CredentialName = 'MsTestSqlDw_7ELE';
 	Credential = $cred;
-	DatabaseHost = 'msprodsqldw.database.windows.net';
+	DatabaseHost = 'mstestsqldw.database.windows.net';
 	Port = 1433
 }
-$createParams2 = @{
-	Account = 'mscrmprodadla';
-	DatabaseName = 'master';
-	CredentialName = 'gpink003_instance';
-	Credential = $cred;
-	DatabaseHost = 'msprodsqldw.database.windows.net';
-	Port = 1433
-}
+New-AzureRmDataLakeAnalyticsCatalogCredential @credParams
+
+<#
 $removeParams = @{
 	Account = 'mscrmprodadla';
 	DatabaseName = 'master';
-	Name = 'gpink003';
+	Name = 'MsTestSqlDw-7ELE';
+	Force = $true;
+	Password = $cred
 }
 Remove-AzureRmDataLakeAnalyticsCatalogCredential @removeParams
-Start-Sleep -Seconds 3
-New-AzureRmDataLakeAnalyticsCatalogCredential @createParams2
+#>
