2a8ee5a8f357220f1a36aa5a4c3738d40656fe1e
Graham Pinkston <graham.pinkston@gmail.com>
2018-02-23 10:21:53 -0600

    init

---
+++ DataLake.Analytics/SQL/shrink-log.sql
@@ -0,0 +1,30 @@
+USE									[7ELE]
+GO
+IF EXISTS							(SELECT * FROM sys.procedures WHERE [name] = 'usp_Shrink_Log')
+BEGIN
+DROP PROCEDURE						[dbo].[usp_Shrink_Log]
+END
+GO
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE					[dbo].[usp_Shrink_Log]
+AS
+SET NOCOUNT ON
+DECLARE								@return_status int
+DBCC SHRINKFILE						(
+									log,
+									0
+									)
+WITH NO_INFOMSGS
+SELECT								[file_id],
+									[name],
+									[type_desc],
+									[size] AS [pages],
+									[max_size] AS [max_pages],
+									([size] * 8) / 1048576 AS [size_GB],
+									([max_size] * 8) / 1048576 AS [max_size_GB]
+FROM								sys.database_files
+WHERE								[file_id] = 2
+SET									@return_status = (SELECT ([size] * 8) / 1048576 FROM sys.database_files WHERE [file_id] = 2)
+RETURN								@return_status
