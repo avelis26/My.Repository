2b45cd21da9c099493a0c2d74dcb090a14888d49
Graham Pinkston <graham.pinkston@gmail.com>
2018-03-18 22:38:03 -0500

    init

--- DataLake.Analytics/PowerShell/Initialize-ForStoreReport.ps1
+++ DataLake.Analytics/PowerShell/Initialize-ForStoreReport.ps1
@@ -3,12 +3,47 @@
 ######################################################
 [CmdletBinding()]
 Param(
-	[parameter(Mandatory = $false)][switch]$autoDate,
-	[parameter(Mandatory = $false)][switch]$test,
-	[parameter(Mandatory = $false)][switch]$scale
+	[parameter(Mandatory = $false)][switch]$size
 )
+[string[]]$emailList = `
+	'graham.pinkston@ansira.com', `
+	'mayank.minawat@ansira.com', `
+	'tyler.bailey@ansira.com'
+$userName = 'gpink003'
+$smtpServer = '10.128.1.125'
+$port = 25
+$fromAddr = 'noreply@7-11.com'
+$sqlServer = 'mstestsqldw.database.windows.net'
+$database = '7ELE'
+$sqlUser = 'sqladmin'
+$databaseSubId = 'da908b26-f6f8-4d61-bf60-b774ff3087ec'
+$sqlPass = Get-Content -Path 'C:\Scripts\Secrets\sqlAdmin.txt' -ErrorAction Stop
+$azuPass = Get-Content -Path "C:\Scripts\Secrets\$userName.cred" -ErrorAction Stop
+$user = $userName + '@7-11.com'
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $(ConvertTo-SecureString -String $azuPass -ErrorAction Stop) -ErrorAction Stop
+Function Create-TimeStamp {
+	[CmdletBinding()]
+	Param(
+		[switch]$forFileName
+	)
+	$now = Get-Date -ErrorAction Stop
+	$day = $now.day.ToString("00")
+	$month = $now.month.ToString("00")
+	$year = $now.year.ToString("0000")
+	$hour = $now.hour.ToString("00")
+	$minute = $now.minute.ToString("00")
+	$second = $now.second.ToString("00")
+	If ($forFileName -eq $true) {
+		$timeStamp = $year + $month + $day + '_' + $hour + $minute + $second
+	}
+	Else {
+		$timeStamp = $year + '/' + $month + '/' + $day + '-' + $hour + ':' + $minute + ':' + $second
+	}
+	Return $timeStamp
+}
 Try {
 # Init
+	$scriptStartTime = Get-Date
 	[System.Threading.Thread]::CurrentThread.Priority = 'Highest'
 	$policy = [System.Net.ServicePointManager]::CertificatePolicy.ToString()
 	If ($policy -ne 'TrustAllCertsPolicy') {
@@ -28,13 +63,14 @@ Try {
 	}
 	Import-Module SqlServer -ErrorAction Stop
 	Import-Module AzureRM -ErrorAction Stop
-	$size = 'p15'
+	Login-AzureRmAccount -Credential $credential -Subscription $databaseSubId -Force -ErrorAction Stop
+# Scale to p15
 	$params = @{
 		SmtpServer = $smtpServer;
 		Port = $port;
 		UseSsl = 0;
 		From = $fromAddr;
-		To = $failEmailList;
+		To = $emailList;
 		BodyAsHtml = $true;
 		Subject = "BITC: Scaling Database to $size";
 		Body = "$(Create-TimeStamp -forFileName)"
@@ -48,11 +84,31 @@ Try {
 		RequestedServiceObjectiveName = $size;
 	}
 	Set-AzureRmSqlDatabase @params
+# Shrik Log
 	Start-Sleep -Seconds 420
-
-
-
-	$sqlTruncateParams = @{
+	$params = @{
+		query = "EXECUTE [dbo].[usp_Shrink_Log]";
+		ServerInstance = $sqlServer;
+		Database = $database;
+		Username = $sqlUser;
+		Password = $sqlPass;
+		QueryTimeout = 0;
+		ErrorAction = 'Stop';
+	}
+	Invoke-Sqlcmd @params
+# Delete Old Data
+	$params = @{
+		SmtpServer = $smtpServer;
+		Port = $port;
+		UseSsl = 0;
+		From = $fromAddr;
+		To = $emailList;
+		BodyAsHtml = $true;
+		Subject = "BITC: Deleting Old Data";
+		Body = "$(Create-TimeStamp -forFileName)"
+	}
+	Send-MailMessage @params
+	$params = @{
 		query = "EXECUTE [dbo].[usp_Delete_Old_Data]";
 		ServerInstance = $sqlServer;
 		Database = $database;
@@ -61,8 +117,70 @@ Try {
 		QueryTimeout = 0;
 		ErrorAction = 'Stop';
 	}
-	Invoke-Sqlcmd @sqlTruncateParams
+	Invoke-Sqlcmd @params
+# Update Statistics
+	Start-Sleep -Seconds 420
+	$params = @{
+		SmtpServer = $smtpServer;
+		Port = $port;
+		UseSsl = 0;
+		From = $fromAddr;
+		To = $emailList;
+		BodyAsHtml = $true;
+		Subject = "BITC: Updating Database Statistics";
+		Body = "$(Create-TimeStamp -forFileName)"
+	}
+	Send-MailMessage @params
+	$params = @{
+		query = "EXECUTE [dbo].[usp_Update_Statistics]";
+		ServerInstance = $sqlServer;
+		Database = $database;
+		Username = $sqlUser;
+		Password = $sqlPass;
+		QueryTimeout = 0;
+		ErrorAction = 'Stop';
+	}
+	Invoke-Sqlcmd @params	
+# Report
+	$scriptEndTime = Get-Date
+	$totTime = New-TimeSpan -Start $scriptStartTime -End $scriptEndTime
+	$message = "Total Run Time----:  $($totTime.Hours.ToString("00")) h $($totTime.Minutes.ToString("00")) m $($totTime.Seconds.ToString("00")) s"
+	$params = @{
+		SmtpServer = $smtpServer;
+		Port = $port;
+		UseSsl = 0;
+		From = $fromAddr;
+		To = $emailList;
+		BodyAsHtml = $true;
+		Subject = "BITC: Database Ready For Store Report";
+		Body = $message;
+	}
+	Send-MailMessage @params
+	$exitCode = 0
 }
 Catch {
-
+	$params = @{
+		SmtpServer = $smtpServer;
+		Port = $port;
+		UseSsl = 0;
+		From = $fromAddr;
+		To = $emailList;
+		BodyAsHtml = $true;
+		Subject = "BITC: ::ERROR:: Store Report Prep Failed!!!";
+		Body = @"
+			<font face='consolas'>
+			Something bad happened!!!<br><br>
+			$($Error[0].CategoryInfo.Activity)<br>
+			$($Error[0].Exception.Message)<br>
+			$($Error[0].Exception.InnerExceptionMessage)<br>
+			$($Error[0].RecommendedAction)<br>
+			$($Error[0].Message)<br>
+			</font>
+"@
+	}
+	Send-MailMessage @params
+	$exitCode = 1
+}
+Finally {
+	[Environment]::Exit($exitCode)
 }
