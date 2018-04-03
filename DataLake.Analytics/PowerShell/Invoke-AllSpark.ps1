8c6fff14abd660e0797dd979e917f9a8d96c8c04
Graham Pinkston <graham.pinkston@gmail.com>
2018-04-03 13:19:16 -0500

    init

--- DataLake.Analytics/PowerShell/Invoke-AllSpark.ps1
+++ DataLake.Analytics/PowerShell/Invoke-AllSpark.ps1
@@ -1,6 +1,6 @@
 # Version  --  v0.9.0.0
 #######################################################################################################
-#
+# Add database maintance feature
 #######################################################################################################
 [CmdletBinding()]
 Param(
@@ -58,12 +58,12 @@ Function Set-AzureSqlDatabaseSize {
 }
 Try {
 	$opsLog = $opsLogRootPath + "$(New-TimeStamp -forFileName)_AllSpark.log"
-	$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $(ConvertTo-SecureString -String $azuPass -ErrorAction Stop) -ErrorAction Stop
-	$message = "Logging into Azure..."
-	Write-Verbose -Message $message
-	Add-Content -Value "$(New-TimeStamp)  $message" -Path $opsLog -ErrorAction Stop
-	Login-AzureRmAccount -Credential $credential -Force -ErrorAction Stop
 	If ($scale.IsPresent -eq $true) {
+		$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $(ConvertTo-SecureString -String $azuPass -ErrorAction Stop) -ErrorAction Stop
+		$message = "Logging into Azure..."
+		Write-Verbose -Message $message
+		Add-Content -Value "$(New-TimeStamp)  $message" -Path $opsLog -ErrorAction Stop
+		Login-AzureRmAccount -Credential $credential -Subscription $databaseSubId -Force -ErrorAction Stop
 		$size = 'P15'
 		$message = "$(Create-TimeStamp)  Scaling database to $size..."
 		Write-Output $message
@@ -75,15 +75,48 @@ Try {
 	}
 # Data to SQL - Store
 	$EtlResult = Invoke-Expression -Command "$AddEjDataToSqlScript -report 's' -autoDate" -ErrorAction Stop
+	If ($EtlResult -ne 0) {
+		$errorParams = @{
+			Message = "$AddEjDataToSqlScript Failed!!!";
+			ErrorId = "01";
+			RecommendedAction = "Fix it.";
+			ErrorAction = "Stop";
+		}
+		Write-Error @errorParams
+	}
 # Data to SQL - CEO
 	$EtlResult = Invoke-Expression -Command "$AddEjDataToSqlScript -report 'c' -autoDate" -ErrorAction Stop
+	If ($EtlResult -ne 0) {
+		$errorParams = @{
+			Message = "$AddEjDataToSqlScript Failed!!!";
+			ErrorId = "02";
+			RecommendedAction = "Fix it.";
+			ErrorAction = "Stop";
+		}
+		Write-Error @errorParams
+	}
 # Data to Hadoop
 	$EtlResult = Invoke-Expression -Command "$AddEjDataToHadoopScript -autoDate" -ErrorAction Stop
+	If ($EtlResult -ne 0) {
+		$errorParams = @{
+			Message = "$AddEjDataToHadoopScript Failed!!!";
+			ErrorId = "03";
+			RecommendedAction = "Fix it.";
+			ErrorAction = "Stop";
+		}
+		Write-Error @errorParams
+	}
 # Remove Old Data
 
 # Rebuild Indexs and stats
 
 # Exit
+	If ($scale.IsPresent -eq $true) {
+		$size = 'P1'
+		Write-Output "Scaling database to size $size..."
+		Add-Content -Value "$(Create-TimeStamp)  Scaling database to size $size..." -Path $opsLog -ErrorAction Stop
+		Set-AzureSqlDatabaseSize -size $size -ErrorAction Stop
+	}
 	$exitCode = 0
 }
 Catch {
@@ -114,29 +147,7 @@ Catch {
 	$exitCode = 1
 }
 Finally {
-	Write-Output 'Finally...'
-	If ($scale.IsPresent -eq $true) {
-		$size = 'P1'
-		Write-Output "Scaling database to size $size..."
-		Add-Content -Value "$(Create-TimeStamp)  Scaling database to size $size..." -Path $opsLog -ErrorAction Stop
-		Set-AzureSqlDatabaseSize -size $size
-	}
 	If ($exit.IsPresent -eq $true) {	
 		[Environment]::Exit($exitCode)	
 	}
 }
-		
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
