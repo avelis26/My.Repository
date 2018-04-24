c5030dc6f8d5c59124542e38632bd6ad2e1a0e88
Graham Pinkston <graham.pinkston@gmail.com>
2018-04-24 16:35:22 -0500

    updates

--- AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
+++ AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
@@ -1,28 +1,17 @@
-﻿Function Create-EncryptedCreds {
-	[CmdletBinding()]
-	Param(
-		#######################################################################################################
-		#######################################################################################################
-		## Enter your 7-11 username without the domain:
-		$userName = 'gpink003'
-		#######################################################################################################
-		#######################################################################################################
-	)
-	Try {
-		$passFileName = "$userName.cred"
-		$passFileRootPath = "C:\Scripts\Secrets\"
-		If (!(Test-Path -LiteralPath $passFileRootPath)) {
-			Write-Verbose -Message "Creating folder:  $passFileRootPath"
-			New-Item -ItemType Directory -Path $passFileRootPath -Force -ErrorAction Stop
-		}
-		$secPass = Read-Host -Prompt "Please type your 7-11 password and hit 'Enter'..." -AsSecureString
-		$encPass = ConvertFrom-SecureString -SecureString $secPass
-		Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName) -ErrorAction Stop
-		Write-Output $($passFileRootPath + $passFileName)
+﻿Try {
+	$userName = Read-Host -Prompt "Please enter your 7-11 username without the domain."
+	$passFileName = "$userName.cred"
+	$passFileRootPath = "C:\Scripts\Secrets\"
+	If (!(Test-Path -LiteralPath $passFileRootPath)) {
+		Write-Verbose -Message "Creating folder:  $passFileRootPath"
+		New-Item -ItemType Directory -Path $passFileRootPath -Force -ErrorAction Stop
 	}
-	Catch {
-		throw $_
-		Write-Host -ForegroundColor Magenta 'Run PowerShell ISE as Administrator'
-	}
+	$secPass = Read-Host -Prompt "Please enter your 7-11 password." -AsSecureString
+	$encPass = ConvertFrom-SecureString -SecureString $secPass
+	Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName) -ErrorAction Stop
+	Write-Output $($passFileRootPath + $passFileName)
 }
-Create-EncryptedCreds -Verbose
+Catch {
+	throw $_
+	Write-Host -ForegroundColor Magenta 'Run PowerShell ISE as Administrator'
+}
