836fc92eaa4c5d1a9c0779ddf1dc4052d1789b65
Graham Pinkston <graham.pinkston@gmail.com>
2018-04-24 16:39:54 -0500

    updates

--- AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
+++ AzureRM.Generic.Tools/Create-EncryptedCreds.ps1
@@ -1,12 +1,12 @@
 ﻿Try {
-	$userName = Read-Host -Prompt "Please enter your 7-11 username without the domain."
+	$userName = Read-Host -Prompt "Please enter your 7-11 username without the domain"
 	$passFileName = "$userName.cred"
 	$passFileRootPath = "C:\Scripts\Secrets\"
 	If (!(Test-Path -LiteralPath $passFileRootPath)) {
 		Write-Verbose -Message "Creating folder:  $passFileRootPath"
 		New-Item -ItemType Directory -Path $passFileRootPath -Force -ErrorAction Stop
 	}
-	$secPass = Read-Host -Prompt "Please enter your 7-11 password." -AsSecureString
+	$secPass = Read-Host -Prompt "Please enter your 7-11 password" -AsSecureString
 	$encPass = ConvertFrom-SecureString -SecureString $secPass
 	Set-Content -Value $encPass -LiteralPath $($passFileRootPath + $passFileName) -ErrorAction Stop
 	Write-Output $($passFileRootPath + $passFileName)
