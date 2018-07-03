99b874c1c3fa9bdc5c8b1eb9fcf3d963427c15f2
Graham Pinkston <graham.pinkston@gmail.com>
2018-07-03 14:31:08 -0500

    v1.0

--- DataLake.Analytics/PowerShell/Export-FirewallConfig.ps1
+++ DataLake.Analytics/PowerShell/Export-FirewallConfig.ps1
@@ -1,6 +1,8 @@
+# v1.0
 # info https://cwiki.apache.org/confluence/download/attachments/30753712/XML_API_Training.pdf?version=1&modificationDate=1366305635000
-$user = 'gpink003'
-$password = 'C:\Scripts\Secrets\firewall.txt'
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $(ConvertTo-SecureString -String $password -AsPlainText -Force)
-$url = 'https://172.22.162.133/api/?type=export&category=configuration&REST_API_TOKEN=1954450954'
-Invoke-RestMethod -Uri $url -Method 'Post' -Credential $credential
+. $($PSScriptRoot + '\Set-SslCertPolicy.ps1')
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+Set-SslCertPolicy
+$key = Get-Content -Path 'C:\Scripts\Secrets\firewall_key.txt'
+$configUrl = "https://172.22.162.133/api/?type=export&category=configuration&key=$key"
+[xml]$config = Invoke-WebRequest -Uri $configUrl
