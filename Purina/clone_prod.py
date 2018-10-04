2ec6458f38dc1a4c2c90e905200b44ebfa2df545
Graham Pinkston <graham.pinkston@gmail.com>
2018-10-04 17:21:25 -0500

    v0.0.5

--- Purina/clone_prod.py
+++ Purina/clone_prod.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# v0.0.4
+# v0.0.5
 # Utility server has python 2.6.9 and 3.4.8
 # https://docs.aws.amazon.com/cli/latest/reference/rds/describe-db-snapshots.html
 # https://docs.aws.amazon.com/cli/latest/reference/rds/restore-db-instance-from-db-snapshot.html
@@ -18,7 +18,7 @@ latest_snapshot_name = parsed_json['DBSnapshotIdentifier']
 print("Latest snapshot for instance: " + str(target_instance_name) + ", is: " + str(latest_snapshot_name), file=open("/home/ec2-user/gpink/groundhog.log", "a"))
 print("Current UTC time is:  " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=open("/home/ec2-user/gpink/groundhog.log", "a"))
 print("Starting restore...", file=open("/home/ec2-user/gpink/groundhog.log", "a"))
-cmd = "aws rds restore-db-instance-from-db-snapshot --db-instance-identifier '" + str(new_instance_name) + "' --db-snapshot-identifier '" + str(latest_snapshot_name) + "'"
+cmd = "aws rds restore-db-instance-from-db-snapshot --db-instance-identifier '" + str(new_instance_name) + "' --db-snapshot-identifier '" + str(latest_snapshot_name) + "' --availability-zone 'us-east-1d'"
 result = subprocess.check_output([cmd], shell=True)
 print("Current UTC time is:  " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=open("/home/ec2-user/gpink/groundhog.log", "a"))
 print("Done :)", file=open("/home/ec2-user/gpink/groundhog.log", "a"))
