e2059275bc97e223465e0cd7525b1bd486749b2f
Graham Pinkston <graham.pinkston@gmail.com>
2018-10-04 16:09:11 -0500

    v0.0.4

--- Purina/clone_prod.py
+++ Purina/clone_prod.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# v0.0.3
+# v0.0.4
 # Utility server has python 2.6.9 and 3.4.8
 # https://docs.aws.amazon.com/cli/latest/reference/rds/describe-db-snapshots.html
 # https://docs.aws.amazon.com/cli/latest/reference/rds/restore-db-instance-from-db-snapshot.html
@@ -12,12 +12,13 @@ target_security_group = 'rds-launch-wizard-4 (sg-cfb6b186)'
 cmd = "aws rds describe-db-snapshots --snapshot-type 'automated' --db-instance-identifier '" + str(target_instance_name) + "' --query='reverse(sort_by(DBSnapshots, &SnapshotCreateTime))[0]'"
 result = subprocess.check_output([cmd], shell=True)
 result = result.decode("utf-8")
+print(result, file=open("/home/ec2-user/gpink/groundhog.log", "a"))
 parsed_json = json.loads(result)
 latest_snapshot_name = parsed_json['DBSnapshotIdentifier']
-print("Latest snapshot for instance: " + str(target_instance_name) + ", is: " + str(latest_snapshot_name))
-print("Current UTC time is:  " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-print("Starting restore...")
+print("Latest snapshot for instance: " + str(target_instance_name) + ", is: " + str(latest_snapshot_name), file=open("/home/ec2-user/gpink/groundhog.log", "a"))
+print("Current UTC time is:  " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=open("/home/ec2-user/gpink/groundhog.log", "a"))
+print("Starting restore...", file=open("/home/ec2-user/gpink/groundhog.log", "a"))
 cmd = "aws rds restore-db-instance-from-db-snapshot --db-instance-identifier '" + str(new_instance_name) + "' --db-snapshot-identifier '" + str(latest_snapshot_name) + "'"
 result = subprocess.check_output([cmd], shell=True)
-print("Current UTC time is:  " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-print("Done :)")
+print("Current UTC time is:  " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file=open("/home/ec2-user/gpink/groundhog.log", "a"))
+print("Done :)", file=open("/home/ec2-user/gpink/groundhog.log", "a"))
