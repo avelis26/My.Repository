21deaccb6933cdaf7e371e850d71ee95b45d5f94
Graham Pinkston <graham.pinkston@gmail.com>
2018-10-22 15:28:09 -0500

    init

---
+++ Server.Firewall/Test-WindowsUpdateDomain.ps1
@@ -0,0 +1,19 @@
+Clear-Host
+$outFile = 'C:\tmp\domain.txt'
+Set-Content -Path $outFile -Value $(Get-Date -Format 'yyyy/MM/dd_HH:mm:ss')
+$domains = `
+'windowsupdate.microsoft.com', `
+'update.microsoft.com', `
+'windowsupdate.com', `
+'download.windowsupdate.com', `
+'download.microsoft.com', `
+'test.stats.update.microsoft.com', `
+'ntservicepack.microsoft.com', `
+'ods.opinsights.azure.com', `
+'oms.opinsights.azure.com', `
+'blob.core.windows.net', `
+'azure-automation.net'
+ForEach ($domain in $domains) {
+    $ip = Resolve-DnsName -Name $domain -DnsOnly -TcpOnly
+    #Add-Content -Path $outFile -Value $ip[2].IPAddress
+}
