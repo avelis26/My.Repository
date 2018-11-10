62faa78f4fb8348c48ef8c0e2b052b25f3bfbf43
Graham Pinkston <graham.pinkston@gmail.com>
2018-11-09 18:34:00 -0800

    removed temp stuff

--- Server.Firewall/Create-WindowsUpdatePubIpFile.ps1
+++ Server.Firewall/Create-WindowsUpdatePubIpFile.ps1
@@ -1,5 +1,6 @@
-#$outFile = 'C:\inetpub\wwwroot\MWUS.txt'
-$outFile = 'C:\tmp\MWUS.txt'
+Clear-Host
+$outFile = 'C:\inetpub\wwwroot\MWUS.txt'
+$dnss = '1.1.1.1', '208.67.222.222', '195.46.39.39', '8.8.8.8'
 $domains = `
 'windowsupdate.microsoft.com', `
 'update.microsoft.com', `
@@ -12,36 +13,39 @@ $domains = `
 'oms.opinsights.azure.com', `
 'blob.core.windows.net', `
 'azure-automation.net'
-[string[]]$ipList = $null
-ForEach ($domain in $domains) {
-	$responses = Resolve-DnsName -Name $domain
-	ForEach ($response in $responses) {
-		If ($response.Type -eq 'A') {
-			$ipList += $($response.Address)
-		}
-		ElseIf ($response.Type -eq 'CNAME') {
-			$ipList += $(Resolve-DnsName -Name $response.NameHost).IP4Address
-		}
-		ElseIf ($response.Type -eq 'SOA') {
-			$results = Resolve-DnsName -Name $response.PrimaryServer
-			ForEach ($result in $results) {
-				If ($result.Type -eq 'A') {
-					$ipList += $result.IPAddress
+ForEach ($dns in $dnss) {
+	[string[]]$ipList = $null
+	ForEach ($domain in $domains) {
+		$responses = Resolve-DnsName -Name $domain -Server $dns
+		ForEach ($response in $responses) {
+			If ($response.Type -eq 'A') {
+				$ipList += $($response.Address)
+			}
+			ElseIf ($response.Type -eq 'CNAME') {
+				$ipList += $(Resolve-DnsName -Name $response.NameHost -Server $dns).IP4Address
+			}
+			ElseIf ($response.Type -eq 'SOA') {
+				$results = Resolve-DnsName -Name $response.PrimaryServer -Server $dns
+				ForEach ($result in $results) {
+					If ($result.Type -eq 'A') {
+						$ipList += $result.IPAddress
+					}
+				}
+				$results = Resolve-DnsName -Name $response.NameAdministrator -Server $dns
+				ForEach ($result in $results) {
+					If ($result.Type -eq 'A') {
+						$ipList += $result.IPAddress
+					}
 				}
 			}
-			$results = Resolve-DnsName -Name $response.NameAdministrator
-			ForEach ($result in $results) {
-				If ($result.Type -eq 'A') {
-					$ipList += $result.IPAddress
-				}
+			Else {
+				Write-Host -ForegroundColor Yellow "UNKNOWN RECORD TYPE: $($response.Type)"
 			}
-		}
-		Else {
-			Write-Host -ForegroundColor Yellow "UNKNOWN RECORD TYPE: $($response.Type)"
 		}
 	}
+	$content = Get-Content -Path $outFile -ErrorAction SilentlyContinue
+	$output = $ipList + $content
+	$final = $output | Sort-Object | Get-Unique
+	Set-Content -Path $outFile -Value $final
 }
-$content = Get-Content -Path $outFile -ErrorAction SilentlyContinue
-$output = $ipList + $content
-$final = $output | Sort-Object | Get-Unique
-Set-Content -Path $outFile -Value $final
+Write-Output 'Done'
