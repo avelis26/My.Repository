1fece66b58badca82905e60c35fea6f84da190c6
Graham Pinkston <graham.pinkston@gmail.com>
2019-03-14 07:39:25 -0500

    added comment

--- SCVL/Create-SelfSignCert.ps1
+++ SCVL/Create-SelfSignCert.ps1
@@ -1,3 +1,5 @@
+# https://medium.com/the-new-control-plane/generating-self-signed-certificates-on-windows-7812a600c2d8
+
 $cert = New-SelfSignedCertificate -certstorelocation 'cert:\localmachine\my' -dnsname 'CN=ansirascvl.com' -notafter $($(Get-Date).AddYears(3))
 $pwd = ConvertTo-SecureString -String 'Pkh5N.>EnHF{!75f' -Force -AsPlainText
 $path = 'cert:\localMachine\my\' + $cert.thumbprint
