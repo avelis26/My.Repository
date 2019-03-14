41f9bfce0a6c53270b90e268aa68d5597de600dc
graham.pinkston@ansira.com <graham.pinkston@ansira.com>
2019-03-14 12:23:26 -0500

    added subject

--- SCVL/Create-SelfSignCert.ps1
+++ SCVL/Create-SelfSignCert.ps1
@@ -1,4 +1,4 @@
-$cert = New-SelfSignedCertificate -certstorelocation 'cert:\localmachine\my' -dnsname 'CN=ansirascvl.com' -notafter $($(Get-Date).AddYears(3))
+$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -DnsName 'ansirascvl.com' -Subject 'ansirascvl.com' -NotAfter $($(Get-Date).AddYears(3))
 $pwd = ConvertTo-SecureString -String 'Pkh5N.>EnHF{!75f' -Force -AsPlainText
 $path = 'cert:\localMachine\my\' + $cert.thumbprint
 Export-PfxCertificate -cert $path -FilePath 'c:\temp\ansirascvl.pfx' -Password $pwd
