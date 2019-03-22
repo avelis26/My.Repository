a3701bd3023c9b11a04d854237b01261fcfb31fb
Graham Pinkston <graham.pinkston@gmail.com>
2019-03-22 13:16:10 -0500

    v1.0

--- SCVL/Create-SelfSignCert.ps1
+++ SCVL/Create-SelfSignCert.ps1
@@ -1,12 +1,5 @@
 # https://medium.com/the-new-control-plane/generating-self-signed-certificates-on-windows-7812a600c2d8
 $cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -DnsName 'ansirascvl.com' -Subject 'ansirascvl.com' -NotAfter $($(Get-Date).AddYears(3))
-$pwd = ConvertTo-SecureString -String 'Pkh5N.>EnHF{!75f' -Force -AsPlainText
+$pwd = ConvertTo-SecureString -String 'ansira' -Force -AsPlainText
 $path = 'cert:\localMachine\my\' + $cert.thumbprint
 Export-PfxCertificate -cert $path -FilePath 'c:\temp\ansirascvl.pfx' -Password $pwd
-
-
-
-$lifetime=Get-Date
-New-SelfSignedCertificate -Subject contoso100.com `
-  -NotAfter $lifetime.AddDays(365) -KeyUsage DigitalSignature, KeyEncipherment `
-  -Type SSLServerAuthentication -DnsName *.contoso100.com, contoso100.com
