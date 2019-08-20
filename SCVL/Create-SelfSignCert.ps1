8712b8e9a5abf59883a4ce981f211614678b6a44
Graham Pinkston <Graham.Pinkston@Ansira.com>
2019-08-20 10:20:59 -0500

    added second admin key to scvlsftp user

--- SCVL/Create-SelfSignCert.ps1
+++ SCVL/Create-SelfSignCert.ps1
@@ -1,5 +1,8 @@
 # https://medium.com/the-new-control-plane/generating-self-signed-certificates-on-windows-7812a600c2d8
-$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -DnsName 'ansirascvl.com' -Subject 'ansirascvl.com' -NotAfter $($(Get-Date).AddYears(3))
-$pwd = ConvertTo-SecureString -String 'ansira' -Force -AsPlainText
+#$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -DnsName 'ansirascvl.com' -Subject 'ansirascvl.com' -NotAfter $($(Get-Date).AddYears(3))
+
+
+$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -Subject 'Azure VPN' -NotAfter $($(Get-Date).AddYears(10))
+$pwd = ConvertTo-SecureString -String '1ansira' -Force -AsPlainText
 $path = 'cert:\localMachine\my\' + $cert.thumbprint
 Export-PfxCertificate -cert $path -FilePath 'c:\temp\ansirascvl.pfx' -Password $pwd
