26db4ca74ccdfaba0e87890a011af80a7c037ff6
Graham Pinkston <Graham.Pinkston@ansira.com>
2019-08-26 17:13:47 -0500

    init

--- SCVL/Enable-AcceleratedNetworking.ps1
+++ SCVL/Enable-AcceleratedNetworking.ps1
@@ -12,4 +12,6 @@ $user = $userName + $userDomain
 $azuPass = Get-Content -Path "C:\Scripts\Secrets\$userName.cred"
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $(ConvertTo-SecureString -String $azuPass)
 Connect-AzAccount -Credential $credential -Subscription $subscriptionId
-Get-AzNetworkInterface | Where-Object -FilterScript {$_.VirtualMachine.Id -like "*$vmName*"}
+$nic = Get-AzNetworkInterface | Where-Object -FilterScript {$_.VirtualMachine.Id -like "*$vmName*"}
+$nic.EnableAcceleratedNetworking = $true
+Set-AzNetworkInterface -NetworkInterface $nic
