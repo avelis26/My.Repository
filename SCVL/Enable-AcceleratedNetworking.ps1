5de266a710693bcbc26ddaa753e5da1fe0143431
Graham Pinkston <Graham.Pinkston@ansira.com>
2019-08-26 17:12:54 -0500

    init

---
+++ SCVL/Enable-AcceleratedNetworking.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory=$False)] [string]$subscriptionId = 'f6915c04-23d0-4ac4-8cd7-b69ac91c2ab8',
+	[Parameter(Mandatory=$False)] [string]$resourceGroupName = 'PROD-Networking-rsg',
+    [Parameter(Mandatory=$False)] [string]$userName = 'graham.pinkston',
+    [Parameter(Mandatory=$False)] [string]$userDomain = '@ansira.com',
+    [Parameter(Mandatory=$False)] [string]$vmName = 'ScvlProdSftp01'
+)
+$ErrorActionPreference = 'Stop'
+Import-Module Az
+$user = $userName + $userDomain
+$azuPass = Get-Content -Path "C:\Scripts\Secrets\$userName.cred"
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $(ConvertTo-SecureString -String $azuPass)
+Connect-AzAccount -Credential $credential -Subscription $subscriptionId
+Get-AzNetworkInterface | Where-Object -FilterScript {$_.VirtualMachine.Id -like "*$vmName*"}
