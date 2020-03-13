41e3e1db74f60b61b41d80a7c88f101544647496
Graham Pinkston <Graham.Pinkston@Ansira.com>
2020-03-13 16:18:59 -0500

    minor updates

--- SCVL/Create-SelfSignCert.ps1
+++ SCVL/Create-SelfSignCert.ps1
@@ -2,7 +2,7 @@
 #$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -DnsName 'ansirascvl.com' -Subject 'ansirascvl.com' -NotAfter $($(Get-Date).AddYears(3))
 
 
-$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -Subject 'Azure VPN' -NotAfter $($(Get-Date).AddYears(10))
-$pwd = ConvertTo-SecureString -String '1ansira' -Force -AsPlainText
+$cert = New-SelfSignedCertificate -CertStoreLocation 'cert:\localmachine\my' -Subject '*.ansirascvl.com' -NotAfter $($(Get-Date).AddYears(3))
+$pwd = ConvertTo-SecureString -String $(Get-Content -Path 'C:\tmp\secret.txt') -Force -AsPlainText
 $path = 'cert:\localMachine\my\' + $cert.thumbprint
-Export-PfxCertificate -cert $path -FilePath 'c:\temp\ansirascvl.pfx' -Password $pwd
+Export-PfxCertificate -cert $path -FilePath 'c:\tmp\ansira_scvl_ldap.pfx' -Password $pwd
