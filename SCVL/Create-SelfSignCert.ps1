d4561e1c12f4218e67eed5da2bf41e2a5cf823e5
Graham Pinkston <Graham.Pinkston@Ansira.com>
2020-03-18 17:11:24 -0500

    v1.0

--- SCVL/Create-SelfSignCert.ps1
+++ SCVL/Create-SelfSignCert.ps1
@@ -1,5 +1,13 @@
 # https://docs.microsoft.com/en-us/azure/active-directory-domain-services/tutorial-configure-ldaps
 # https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site
+# v1.0 - Graham Pinkston
+# Secret file should contain password to be used for encrypting keys
+# Be sure to populate the paths for where you want the files to save
+# The CER file is for grabbing the pub key for populating the Azure portal
+# Default expiration date applied is today + 3 years
+# 
+# 
+# # Variables
 $years = 3
 $password = ConvertTo-SecureString -String $(Get-Content -Path 'C:\Users\Avelis\OneDrive - Ansira.com\Secrets\tmp\secret.txt') -Force -AsPlainText
 $certPath = 'Cert:\CurrentUser\My\'
@@ -7,6 +15,8 @@ $ldapPath = 'C:\Users\Avelis\OneDrive - Ansira.com\Secrets\tmp\Ansira_LDAP_Azure
 $cerPath = 'C:\Users\Avelis\OneDrive - Ansira.com\Secrets\tmp\Ansira_P2S_Azure_Root.cer'
 $rootPath = 'C:\Users\Avelis\OneDrive - Ansira.com\Secrets\tmp\Ansira_P2S_Azure_Root.pfx'
 $childPath = 'C:\Users\Avelis\OneDrive - Ansira.com\Secrets\tmp\Ansira_P2S_Azure_Child.pfx'
+
+# LDAP Root Cert
 $params = @{
 	CertStoreLocation = $certPath;
 	Subject = '*.ansirascvl.com';
@@ -14,6 +24,8 @@ $params = @{
 }
 $certificate = New-SelfSignedCertificate @params
 Export-PfxCertificate -cert $($certPath + $certificate.thumbprint) -FilePath $ldapPath -Password $password
+
+# VPN Root Cert
 $params = @{
 	Type = 'Custom';
 	KeySpec = 'Signature';
@@ -29,6 +41,8 @@ $params = @{
 $certificate = New-SelfSignedCertificate @params
 Export-PfxCertificate -cert $($certPath + $certificate.thumbprint) -FilePath $rootPath -Password $password
 Export-Certificate -cert $($certPath + $certificate.thumbprint) -FilePath $cerPath
+
+# VPN Child Cert
 $params = @{
 	Type =  'Custom';
 	DnsName = 'Ansira_P2S_Azure_Child';
