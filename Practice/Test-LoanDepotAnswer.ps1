5aa88dc96dd45ed9ce193110c93a7480a09b4be3
Graham Pinkston <Graham.Pinkston@Ansira.com>
2020-04-04 17:34:41 -0500

    init

---
+++ Practice/Test-LoanDepotAnswer.ps1
@@ -0,0 +1,14 @@
+
+[cmdletbinding()]
+Function Test-IsMultipleOf {
+	param(
+		[Parameter(Mandatory=$true)]
+		[int]
+		$inputValue
+	)
+	$num = $inputValue
+	while ($n -gt 0) {
+		$num = $num
+	}
+}
+$range = 1..10
