03441b94e4dd089e593c40e6ccbab49d99366630
Graham Pinkston <Graham.Pinkston@Ansira.com>
2020-04-04 18:36:55 -0500

    v1.0.0

--- Practice/Test-IsMultipleOf.ps1
+++ Practice/Test-IsMultipleOf.ps1
@@ -1,3 +1,28 @@
+<#
+  .SYNOPSIS
+  This script is the answer to a pre-interview question asked my Loan Depot.
+
+  .DESCRIPTION
+  The original question asked was: "If we list all the natural numbers below 10
+  that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these 
+  multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000."
+
+  .NOTES
+  Version 1.0.0
+
+  .INPUTS
+  Inputs are hard coded values.
+
+  .OUTPUTS
+  The sum of a list of natrual numbers.
+
+  .EXAMPLE
+  C:\PS> .\Test-IsMultipoleOf.ps1
+#>
+Clear-Host
+$multiples = 3, 5
+$range = 1..999
+$sum = 0
 Function Test-IsMultipleOf {
 	param(
 		[Parameter(Mandatory=$true)]
@@ -18,17 +43,14 @@ Function Test-IsMultipleOf {
 		Write-Output -InputObject $false
 	}
 }
-Clear-Host
-$multiples = 3, 5
-$range = 1..999
-$results = @()
 ForEach ($number in $range) {
 	ForEach ($multiple in $multiples) {
 		if ($(Test-IsMultipleOf -inputValue $number -multipleValue $multiple) -eq $true) {
 			Write-Host -BackgroundColor Black -ForegroundColor Green "$number is a multiple of $multiple."
-			$results += $number
+			$sum += $number
 		} else {
 			Write-Host -BackgroundColor Black -ForegroundColor Red "$number is NOT a multiple of $multiple."
 		}
 	}
 }
+Write-Host -BackgroundColor Black -ForegroundColor Cyan "The sum of all the multiples of 3 OR 5 found between 1 and 999 is $($sum.ToString('N0'))."
