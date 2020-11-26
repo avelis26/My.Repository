3afc5f3f2697fcdd2aa77d414ebbfb4284d3692b
Graham Pinkston <graham.pinkston@gmail.com>
2020-11-25 23:09:25 -0600

    init

--- Practice/Query-Repeaterbook.py
+++ Practice/Query-Repeaterbook.py
@@ -1,1 +1,4 @@
-print('hello world')
+import sys
+print(sys.version)
+gps_loc = '32.917354, -97.328044'
+prox_url = 'https://www.repeaterbook.com/repeaters/prox_result.php?city=&lat=32.917354&long=-97.328019&distance=90&Dunit=m&band1=14&band2=4&freq=&call=&features%5B%5D=FM&features%5B%5D=YSF&features%5B%5D=WIRES&features%5B%5D=WX&status_id=%25&use=%25&order=distance_calc%2C+state_id%2C+%60call%60+ASC'
