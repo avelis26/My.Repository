0bdd3b000f4d9c7a2d0b001c61fa82e2b910cb0b
Graham Pinkston <graham.pinkston@gmail.com>
2020-12-29 08:45:27 -0600

    minor changes

--- Practice/Query-Repeaterbook.py
+++ Practice/Query-Repeaterbook.py
@@ -1,4 +1,5 @@
 import sys
 print(sys.version)
-gps_loc = '32.917354, -97.328044'
+gps_loc = '32.917343, -97.328009'
 prox_url = 'https://www.repeaterbook.com/repeaters/prox_result.php?city=&lat=32.917354&long=-97.328019&distance=90&Dunit=m&band1=14&band2=4&freq=&call=&features%5B%5D=FM&features%5B%5D=YSF&features%5B%5D=WIRES&features%5B%5D=WX&status_id=%25&use=%25&order=distance_calc%2C+state_id%2C+%60call%60+ASC'
+print(prox_url)
