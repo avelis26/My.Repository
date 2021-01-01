40e3aa27e689857ba8c38c55756a614911eefbb3
Graham Pinkston <graham.pinkston@gmail.com>
2020-12-31 19:44:15 -0600

    updates

--- Lexipol/config_ssh.sh
+++ Lexipol/config_ssh.sh
@@ -4,6 +4,21 @@ sudo nano /etc/ssh/sshd_config
 # PasswordAuthentication yes
 # ChallengeResponseAuthentication yes
 # PubkeyAuthentication yes
+# UsePAM yes
 sudo service sshd restart
 
-clear && sudo cat /etc/ssh/sshd_config | grep 'PasswordAuthentication\|ChallengeResponseAuthentication\|PubkeyAuthentication'
+clear && sudo cat /etc/ssh/sshd_config | grep 'PasswordAuthentication\|ChallengeResponseAuthentication\|PubkeyAuthentication\|UsePAM'
+
+sudo tail -f /var/log/secure
+
+sudo su
+file=/etc/ssh/sshd_config
+cat $file > $file.old
+awk '
+$1=="PasswordAuthentication" {$2="yes"}
+$1=="ChallengeResponseAuthentication" {$2="yes"}
+$1=="PubkeyAuthentication" {$2="yes"}
+$1=="UsePAM" {$2="yes"}
+{print}
+' $file.old > $file
+diff $file.old $file
