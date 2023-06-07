4160c426794b6c72374404d1fb04b461927b06bd
Graham Pinkston <graham.pinkston@gmail.com>
2023-06-07 15:41:45 -0500

    Added try catch finally and pivot var

--- Practice/New-Practice.ps1
+++ Practice/New-Practice.ps1
@@ -1,25 +1,36 @@
-$total = 78
+$begin = Get-Date
+$total = 9999
+$min = 0
+$max = 999999
+$pivot = 555555
 $i = 0
 $above = @()
 $below = @()
-While ($i -lt $total) {
-	$number = Get-Random -Minimum 0 -Maximum 999
-	Write-Host $number -NoNewline
-	If ($number -lt 500) {
-		$below += $number
-		Write-Host -ForegroundColor Red "	The number is below 500"
-	} Else {
-		$above += $number
-		Write-Host -ForegroundColor Green "	The number is above 500"
-	}
-	$i++
-}
-function Get-Percentage {
-	param (
+Function Get-Percentage {
+	Param (
 		[int]$value,
 		[int]$total
 	)
 	Return $(($value / $total) * 100).ToString("#.#")
 }
-Write-Host -ForegroundColor Red "The below count was: $($below.Count) out of $total, or $(Get-Percentage -value $below.Count -total $total)%"
-Write-Host -ForegroundColor Green "The above count was: $($above.Count) out of $total, or $(Get-Percentage -value $above.Count -total $total)%"
+Try {
+	While ($i -lt $total) {
+		$number = Get-Random -Minimum $min -Maximum $max
+		Write-Host $number -NoNewline
+		If ($number -lt $pivot) {
+			$below += $number
+			Write-Host -ForegroundColor Red "	The number is below $pivot"
+		} Else {
+			$above += $number
+			Write-Host -ForegroundColor Green "	The number is above $pivot"
+		}
+		$i++
+	}
+	Write-Host -ForegroundColor Red "The below count was: $($below.Count) out of $total, or $(Get-Percentage -value $below.Count -total $total)%"
+	Write-Host -ForegroundColor Green "The above count was: $($above.Count) out of $total, or $(Get-Percentage -value $above.Count -total $total)%"
+} Catch {
+	Write-Error "Something went wrong!!!"
+} Finally {
+	$runTime = New-TimeSpan -Start $begin -End $(Get-Date)
+	Write-Host -ForegroundColor Cyan "The run time was: $runTime"
+}
