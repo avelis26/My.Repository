895b80787315391bb4bdcedf25841262b9c77910
Graham Pinkston <graham.pinkston@gmail.com>
2023-07-19 16:19:10 -0500

    Testing APIs with PS

--- Practice/Get-WeatherForecast.ps1
+++ Practice/Get-WeatherForecast.ps1
@@ -1,14 +1,18 @@
-Clear-Host
+[CmdletBinding()]
+# Free account API, nothing special.
 $apiKey = 'fc82f9f1916e4cb4856202622231907'
 $zipCode = '65536'
-$days = '1'
+$days = '2'
 $uri = "http://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$zipCode&days=$days&aqi=no&alerts=no"
+Clear-Host
 if (!$results) {
     Write-Host -ForegroundColor Green "Calling API..."
     $results = Invoke-RestMethod -Method Get -Uri $uri
 }
 $today = $results.forecast.forecastday[0].day.maxtemp_f
 $tmrw = $results.forecast.forecastday[1].day.maxtemp_f
+Write-Debug "Today: $today"
+Write-Debug "TMRW: $tmrw"
 switch ($true) {
     { $tmrw -gt $today } { Write-Host -ForegroundColor Red "Tomorrow will be hotter than today" }
     { $tmrw -eq $today } { Write-Host -ForegroundColor Gray "Tomorrow will be equal to today" }
